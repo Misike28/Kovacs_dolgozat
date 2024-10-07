e74a57406f92a8ab416f5d192f8b377b9fb24d96
Kovács Mihály <117653038+Misike28@users.noreply.github.com>
2024-10-07 10:23:10 +0200

    Add files via upload

---
+++ kovacs_mihaly_dolgozat/kovacs_mihaly_dolgozat/Program.cs
@@ -0,0 +1,39 @@
+﻿using kovacs_mihaly_dolgozat;
+using System.Text;
+
+List<Versenyzok> racers = [];
+using StreamReader sr = new(
+    path: "..\\..\\..\\src\\forras.txt",
+    encoding: Encoding.UTF8);
+
+while (!sr.EndOfStream) racers.Add(new(sr.ReadLine()));
+Console.WriteLine($"{racers.Count} versenyző fejezte be a versenyt");
+
+var elit = racers.Count(r => r.Category == "elit");
+Console.WriteLine($"{elit} számú versenyző van az elit kategóriában");
+
+var kor = racers
+    .Where (r=> r.Sex==false)
+    .Average (r=> DateTime.Now.Year - r.Year);
+
+Console.WriteLine($"A női versenyzők átlag életkora: {kor}");
+var kereksum = TimeSpan.FromSeconds(racers.Sum(p => p.Kerekpar.TotalSeconds)).TotalHours;
+Console.WriteLine($"A versenyzők kerékpározással töltött összideje: {kereksum:0.00} óra");
+
+var uszasavg = TimeSpan.FromSeconds(racers
+    .Where(r => r.Category == "elit junior")
+    .Average(r => r.Uszas.TotalSeconds));
+Console.WriteLine($"Az úszással töltött átlag idő: {uszasavg}");
+
+var ferfiwinner = racers
+    .Where(r => r.Sex)
+    .MinBy(r => r.Uszas.TotalSeconds + r.Elsodepo.TotalSeconds + r.Uszas.TotalSeconds + r.Masodikdepo.TotalSeconds + r.Futas.TotalSeconds);
+Console.WriteLine(ferfiwinner);
+var kategoriak = racers
+    .OrderBy(r => r.Category)
+    .GroupBy(r => r.Category);
+foreach (var c in kategoriak)
+{
+    Console.WriteLine($"{c.Key}: {c.Count()}");
+}
+
---
+++ kovacs_mihaly_dolgozat/kovacs_mihaly_dolgozat/Versenyzok.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kovacs_mihaly_dolgozat
+{
+    internal class Versenyzok
+    {
+        public string Name { get; set; }
+        public int Year { get; set; }
+        public int Rajtszam { get; set; }
+        public bool Sex { get; set; }
+        public string Category { get; set; }
+        public TimeSpan Uszas { get; set; }
+        public TimeSpan Elsodepo { get; set; }
+        public TimeSpan Kerekpar { get; set; }
+        public TimeSpan Masodikdepo { get; set; }
+        public TimeSpan Futas { get; set; }
+
+      
+
+        public Versenyzok(string row) {
+            var v = row.Split(";");
+            Name = v[0];
+            Year=int.Parse(v[1]);
+            Rajtszam = int.Parse(v[2]);
+            if (v[3] == "f")
+            {
+                Sex= true;
+            }
+            else
+            {
+                Sex= false;
+            }
+            Category = v[4];
+            Uszas = TimeSpan.Parse(v[5]);
+            Elsodepo = TimeSpan.Parse(v[6]);
+            Kerekpar = TimeSpan.Parse(v[7]);
+            Masodikdepo = TimeSpan.Parse(v[8]);
+            Futas = TimeSpan.Parse(v[9]);
+
+        }
+        public override string ToString() =>
+            $"{Name} - {(Sex ? "Férfi":"Nő")}: ({Year}-ben született, {Rajtszam}-s rajtszámmal. {Category} kategóriában. Idejei:" +
+           $"\n {Uszas}\t {Elsodepo}\t {Kerekpar}\t {Masodikdepo}\t {Futas}";
+
+    }
+}
